ec11ef105ef73b7e10c35a65466e3028c5b43996
Javier Antonio Frias <jabfryyy@gmail.com>
2022-03-26 03:40:03 -0300

    Mi repo en beta

---
+++ js/MyScript.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+window.onscroll = function () {
+    //Llamamos a la funcion al hacer scroll en la pagina
+    scrollable();
+};
+function scrollable() {
+    var y = 0;
+    var currentY = window.pageYOffset; //pageYOffset es un alias, devuelve los pixeles scrolleados en Y
+    if (currentY > y) { //Si la cantidad de pixeles recorridos en Y es mayor a 0 (variable y)...
+        document.querySelector('#MyNavBar').classList.remove('myNavBar');
+        document.querySelector("#MyNavBar").classList.add("fixed-theme");
+    }
+    else {
+        if (y == currentY) {
+            document.querySelector('#MyNavBar').classList.add('myNavBar', 'fixed-theme');
+        }
+    }
+}
+
+scrollable();
